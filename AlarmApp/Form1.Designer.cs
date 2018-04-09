f3378a896dbeec46965f8a57399e2e312515cc4b
mertnuhuz <mertnuhuz@gmail.com>
2018-04-10 01:42:15 +0300

    Uygulama kurulumu

---
+++ AlarmApp/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace AlarmApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.kronometre = new System.Windows.Forms.Timer(this.components);
+            this.dakikaAyarla = new System.Windows.Forms.NumericUpDown();
+            this.yenidenKur = new System.Windows.Forms.Button();
+            this.onDakikaButton = new System.Windows.Forms.Button();
+            this.otuzDakikaButton = new System.Windows.Forms.Button();
+            this.saat = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dakikaAyarla)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // kronometre
+            // 
+            this.kronometre.Tick += new System.EventHandler(this.kronometre_Tick);
+            // 
+            // dakikaAyarla
+            // 
+            resources.ApplyResources(this.dakikaAyarla, "dakikaAyarla");
+            this.dakikaAyarla.Name = "dakikaAyarla";
+            // 
+            // yenidenKur
+            // 
+            resources.ApplyResources(this.yenidenKur, "yenidenKur");
+            this.yenidenKur.Name = "yenidenKur";
+            this.yenidenKur.UseVisualStyleBackColor = true;
+            this.yenidenKur.Click += new System.EventHandler(this.yenidenKur_Click);
+            // 
+            // onDakikaButton
+            // 
+            resources.ApplyResources(this.onDakikaButton, "onDakikaButton");
+            this.onDakikaButton.Name = "onDakikaButton";
+            this.onDakikaButton.UseVisualStyleBackColor = true;
+            this.onDakikaButton.Click += new System.EventHandler(this.onDakikaButton_Click);
+            // 
+            // otuzDakikaButton
+            // 
+            resources.ApplyResources(this.otuzDakikaButton, "otuzDakikaButton");
+            this.otuzDakikaButton.Name = "otuzDakikaButton";
+            this.otuzDakikaButton.UseVisualStyleBackColor = true;
+            this.otuzDakikaButton.Click += new System.EventHandler(this.otuzDakikaButton_Click);
+            // 
+            // saat
+            // 
+            resources.ApplyResources(this.saat, "saat");
+            this.saat.Name = "saat";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.otuzDakikaButton);
+            this.Controls.Add(this.onDakikaButton);
+            this.Controls.Add(this.yenidenKur);
+            this.Controls.Add(this.dakikaAyarla);
+            this.Controls.Add(this.saat);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dakikaAyarla)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer kronometre;
+        private System.Windows.Forms.NumericUpDown dakikaAyarla;
+        private System.Windows.Forms.Button yenidenKur;
+        private System.Windows.Forms.Button onDakikaButton;
+        private System.Windows.Forms.Button otuzDakikaButton;
+        private System.Windows.Forms.Label saat;
+    }
+}
+
---
+++ AlarmApp/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Media;
+
+namespace AlarmApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            kronometre.Start();
+        }
+
+        private void onDakikaButton_Click(object sender, EventArgs e)
+        {
+            dakikaAyarla.Text = Convert.ToString(alarmKur(10));
+        }
+
+        private void otuzDakikaButton_Click(object sender, EventArgs e)
+        {
+           dakikaAyarla.Text =Convert.ToString(alarmKur(30));
+        }
+
+        private void yenidenKur_Click(object sender, EventArgs e)
+        {
+            kronometre.Start();
+        }
+
+        private void kronometre_Tick(object sender, EventArgs e)
+        {
+            saat.Text = DateTime.Now.ToShortTimeString();
+            if (dakikaAyarla.Value == DateTime.Now.Minute)
+            {
+                kronometre.Stop();
+                SoundPlayer player = new SoundPlayer();
+                string sarkiYol = Application.StartupPath + "/alarm.wav";
+                player.SoundLocation = sarkiYol;
+                player.Play();
+                MessageBox.Show("Vakit geldi!");
+
+            }
+        }
+
+        public decimal alarmKur(int artisMiktari)
+        {
+            decimal vakit = Convert.ToDecimal(Convert.ToInt32(DateTime.Now.Minute) + artisMiktari);
+            if (vakit > 60)
+            {
+                vakit = vakit - 60;
+                return vakit;
+            }
+            else
+            {
+                return vakit;
+            }
+
+        }
+    }
+}
